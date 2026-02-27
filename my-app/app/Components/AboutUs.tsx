b1c667356c92c0631e44a1f3b8999adda88cf970
alikansoh <alikanso.dev@gmail.com>
2026-02-27 13:49:59 +0000

    add service section

--- my-app/app/Components/AboutUs.tsx
+++ my-app/app/Components/AboutUs.tsx
@@ -853,7 +853,7 @@ export default function AboutSection(): JSX.Element {
               <div className="ci-img-main">
                 <div className="ci-img-cover" />
                 <Image
-                  src="/about2.webp"
+                  src="/about2.jpg"
                   alt="Barber giving a precision haircut"
                   fill
                   className="ci-img-el"
